170008d1c445144036568709c5c47003b2af61ba
Arvind P <arvindprabhu02@gmail.com>
2026-01-11 14:35:42 +0530

    Create loan_customers table in credit_risk_schema

---
+++ sql/credit_risk_schema.sql
@@ -0,0 +1,16 @@
+CREATE TABLE loan_customers (
+    ID INTEGER,
+    Age INTEGER,
+    Experience INTEGER,
+    Income INTEGER,
+    ZIPCode INTEGER,
+    Family INTEGER,
+    CCAvg REAL,
+    Education INTEGER,
+    Mortgage INTEGER,
+    Securities_Account INTEGER,
+    CD_Account INTEGER,
+    Online INTEGER,
+    CreditCard INTEGER,
+    Personal_Loan INTEGER
+);
